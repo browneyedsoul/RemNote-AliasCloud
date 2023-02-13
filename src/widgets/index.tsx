31cac5d62c8f006e8dcd31ae0c6ba4bfda72a710
Min-ki Park <browneyedsoul@yonsei.ac.kr>
2023-02-13 12:51:12 +0900

    Fix mobile rendering issue

--- src/widgets/index.tsx
+++ src/widgets/index.tsx
@@ -1,6 +1,17 @@
 import { declareIndexPlugin, ReactRNPlugin } from "@remnote/plugin-sdk";
 
-async function onActivate(plugin: ReactRNPlugin) {}
+let CloudCSS: string;
+
+async function onActivate(plugin: ReactRNPlugin) {
+	await fetch("https://raw.githubusercontent.com/browneyedsoul/RemNote-AliasCloud/main/src/snippet.css")
+    .then((response) => response.text())
+    .then((text) => {
+      CloudCSS = text;
+      console.log("Alias Cloud!");
+    })
+    .catch((error) => console.error(error));
+  await plugin.app.registerCSS("callout", CloudCSS);
+}
 
 async function onDeactivate(_: ReactRNPlugin) {}
 
