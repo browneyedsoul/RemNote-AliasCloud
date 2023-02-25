78bad5c8592584d62c2eaa713438ae1850e32cab
Min-ki Park <browneyedsoul@yonsei.ac.kr>
2023-02-25 12:44:23 +0900

    Fix side effect

--- src/widgets/index.tsx
+++ src/widgets/index.tsx
@@ -3,14 +3,25 @@ import { declareIndexPlugin, ReactRNPlugin } from "@remnote/plugin-sdk";
 let CloudCSS: string;
 
 async function onActivate(plugin: ReactRNPlugin) {
-	await fetch("https://raw.githubusercontent.com/browneyedsoul/RemNote-AliasCloud/main/src/snippet.css")
-    .then((response) => response.text())
-    .then((text) => {
-      CloudCSS = text;
-      console.log("Alias Cloud!");
-    })
-    .catch((error) => console.error(error));
-  await plugin.app.registerCSS("callout", CloudCSS);
+  try {
+    await fetch("snippet.css")
+      .then((response) => response.text())
+      .then((text) => {
+        CloudCSS = text;
+        console.log("Alias Cloud!");
+      })
+      .catch((error) => console.error(error));
+    await plugin.app.registerCSS("alias-cloud", CloudCSS); 
+  } catch (error) {
+    await fetch("https://raw.githubusercontent.com/browneyedsoul/RemNote-AliasCloud/main/src/snippet.css")
+      .then((response) => response.text())
+      .then((text) => {
+        CloudCSS = text;
+        console.log("Alias Cloud from cdn!");
+      })
+      .catch((error) => console.error(error));
+    await plugin.app.registerCSS("alias-cloud-cdn", CloudCSS);
+  }
 }
 
 async function onDeactivate(_: ReactRNPlugin) {}
